af783295593ac0c6bd9f2b9f2e96c34f6a8f91e4
Fibonacci1664 <dave.green1664@yahoo.co.uk>
2019-06-11 12:43:40 +0100

    First commit

---
+++ src/Parameters.cpp
@@ -0,0 +1,55 @@
+/*
+ * Parameters.cpp
+ *
+ *  Created on: 11 Jun 2019
+ *      Author: Dave
+ */
+
+#include<iostream>
+
+using namespace std;
+
+void showMenu()
+{
+	cout << "1. Search" << endl;
+	cout << "2. View Record" << endl;
+	cout << "3. Quit" << endl;
+}
+
+int getInput()
+{
+	cout << "Please choose an option:> " << flush;
+
+	int input;
+	cin >> input;
+
+	return input;
+}
+
+void processSelection(int choice)
+{
+	switch (choice)
+	{
+	case 1:
+		cout << "Searching..." << endl;
+		break;
+	case 2:
+		cout << "Viewing..." << endl;
+		break;
+	case 3:
+		cout << "Quitting..." << endl;
+		break;
+	default:
+		cout << "Oops, something went wrong!" << endl;
+	}
+}
+
+int main()
+{
+	showMenu();
+	int choice = getInput();
+	processSelection(choice);
+
+	return 0;
+}
+
